ebccacfdcf9e46ecc2271e8eb9c153aed1959c61
shahid <shahidulhaq.lone@gmail.com>
2016-06-23 10:37:49 +0530

    commit hdgf

--- parkJockey/contents/locationDetails.js
+++ parkJockey/contents/locationDetails.js
@@ -3,5 +3,9 @@
  */
 (function(){
 	alert("OMG!!!");
+	var page = document.getElementById( "locationDetails" ),
+	changer = document.getElementById( "hsectionchanger" ),
+	sectionChanger;
+	
 	
 })();
